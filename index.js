5d1a283a1d1051944aead6e6bef8f39a89a40f7e
Bakhita87 <bakhitaotieno@gmail.com>
2023-06-11 13:31:02 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,55 @@
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide (){
+
+}
+
+function add(a,b){
+    return a+b
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    return a/b
+}
+
+function addn (){
+    return undefined;
+}
+
+function makeInt(n){
+    return n;
+}
+
+function makeInt(n){
+    return parseInt(n,10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+
+function decrement(b){
+    return b - 1;
+}
+
+function increment (n){
+    return n + 1;
+}
